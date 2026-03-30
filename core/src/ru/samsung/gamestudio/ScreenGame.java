fc3d1f3fe3c6ddad80ef9cddb8c478ccc1531e01
it-cube <it-cube@mck-ktits.ru>
2026-03-30 20:00:09 +0300

    стартовый коммит

---
+++ core/src/ru/samsung/gamestudio/ScreenGame.java
@@ -0,0 +1,89 @@
+package ru.samsung.gamestudio;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+
+public class ScreenGame implements Screen {
+
+    MyGdxGame myGdxGame;
+
+    Bird bird;
+
+    int tubeCount = 3;
+    Tube[] tubes;
+
+    ScreenGame(MyGdxGame myGdxGame) {
+        this.myGdxGame = myGdxGame;
+
+        bird = new Bird(0, 0, 10, 250, 200);
+        tubes = new Tube[tubeCount];
+        for (int i = 0; i < tubeCount; i++) {
+            tubes[i] = new Tube(tubeCount, i);
+        }
+    }
+
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        if (Gdx.input.justTouched()) {
+            bird.onClick();
+        }
+
+        bird.fly();
+
+        ScreenUtils.clear(1, 0, 0, 1);
+        myGdxGame.camera.update();
+        myGdxGame.batch.setProjectionMatrix(myGdxGame.camera.combined);
+        myGdxGame.batch.begin();
+
+        bird.draw(myGdxGame.batch);
+        for (Tube tube : tubes) tube.draw(myGdxGame.batch);
+
+        myGdxGame.batch.end();
+
+        for (Tube tube : tubes) tube.move();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        bird.dispose();
+    }
+
+
+    void initTubes() {
+        tubes = new Tube[tubeCount];
+        for (int i = 0; i < tubeCount; i++) {
+            tubes[i] = new Tube(tubeCount, i);
+        }
+    }
+}
